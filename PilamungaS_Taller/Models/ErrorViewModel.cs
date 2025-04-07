17361dadf5b1c6d21bec0853e0447901e32b04e3
SANTIAGO <SANTIAGO@DESKTOP-BBHOA2A>
2025-04-07 08:04:38 -0500

    Agregar archivos de proyecto.

---
+++ PilamungaS_Taller/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace PilamungaS_Taller.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
